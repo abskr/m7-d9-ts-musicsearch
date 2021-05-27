69892c260f680febb6d58dec4f8aea507adc10e3
ABskr <bskr26@gmx.de>
2021-05-27 16:48:44 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Result, Track } from './types/types'
+
+import Homepage from './components/Homepage'
 
 function App() {
+
+  const [searchInput, setSearchInput] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<Result[]>([])
+  const [track, setTrack] = useState<Track>()
+  
+  const handleInput = (e:any) => {
+    let {value} = e.target
+    setSearchInput(value)
+  }
+
+  const handleSubmit = async(e:any, searchQuery : string = searchInput) => {
+    e.preventDefault()
+    try {
+      const resp = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchQuery}`)
+      if (resp.ok) {
+        const { data } = await resp.json()
+        setSearchResults(data)
+        console.log(searchResults)
+      } else {
+        console.log('ERROR')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getTrack = async(id : number) => {
+    try {
+      const resp = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/track/${id}`)
+      if (resp.ok) {
+        const data = await resp.json()
+        setTrack(data)
+        console.log(track)
+      } else {
+        console.log('ERROR')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route 
+        path='/' 
+        exact 
+        render={(routerProps) => <Homepage {...routerProps} handleInput={handleInput} handleSubmit={handleSubmit} searchInput={searchInput} searchResults={searchResults} getTrack={getTrack} />} />
+    </Router>
   );
 }
 
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,52 @@
+// import { useState } from 'react'
+import styled from 'styled-components'
+import { RouteComponentProps } from 'react-router-dom'
+import { ListGroup } from 'react-bootstrap'
+
+interface HomepageProps {
+  handleInput: any
+  handleSubmit: any
+  searchInput: any
+  searchResults: any
+  getTrack: any
+}
+
+const Homepage = (props: HomepageProps & RouteComponentProps) => {
+
+  return (
+    <div>
+      <SearchBox>
+          <form onSubmit={props.handleSubmit}>
+            <input 
+              type="text"
+              name='searchInput'
+              onChange={props.handleInput}
+              value={props.searchInput} />
+            <button type='submit'>Search</button>
+          </form>
+        </SearchBox>
+        {props.searchResults.length > 0 && 
+        <ListGroup>
+            {props.searchResults.map((result:any) => (
+              <ListGroup.Item key={result.id} onClick={() => props.getTrack(result.id)}>
+                <img src={result.album.cover} alt="Album's cover" />
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
+        }
+      </div>
+  );
+};
+
+const SearchBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 300px;
+  padding: 5px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`;
+
+export default Homepage;
